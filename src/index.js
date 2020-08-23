c5eb54e80a389661695463e9ae13857120885ba5
Henrik Nørgaard <menneskefernis@gmail.com>
2020-08-23 18:37:18 +0200

    Try to use a little linting

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import todo from './todo';
 import elements from './base';
 import project from './project';
 import Projects from './projectsHandler';
-import {ProjectsView, TodosView, DetailsView, AddTodoView, Popup} from './viewHandler';
+import {
+  ProjectsView, TodosView, DetailsView, AddTodoView, Popup,
+} from './viewHandler';
 
 const ProjectController = (() => {
   const addProject = (e) => {
@@ -10,41 +12,33 @@ const ProjectController = (() => {
     const projectName = ProjectsView.getFormInput();
     const proj = project(projectName);
     Projects.add(proj);
-    
     ProjectsView.render(Projects.get());
     openProject(proj);
     elements.addProjectForm.reset();
     Projects.saveToLocalStorage();
-  }
-  
+  };
   const openProject = (input) => {
     let project;
     input.target ? project = findProjectOnEvent(input) : project = input;
-    
     Projects.setActive(project);
     ProjectsView.select(project);
-    
     TodosView.render(project.todos);
     TodoController.closeTodo();
-  }
-  
-  const findProjectOnEvent = e => {
+  };
+  const findProjectOnEvent = (e) => {
     const target = e.target;
-    if (!target.matches('.project, .project *')) return; 
-      
+    if (!target.matches('.project, .project *')) return;
     const id = target.closest('li').dataset.id;
     return Projects.find(id);
-  }
-  
+  };
   const deleteProject = (target) => {
     if (!target.matches('.del-project-btn, .del-project-btn *')) return;
     const id = target.closest('li').dataset.id;
-    
     Projects.remove(id);
     ProjectsView.render(Projects.get());
     TodosView.render();
     Projects.saveToLocalStorage();
-  }
+  };
 
   return {addProject, deleteProject, openProject};
 })();
@@ -53,9 +47,16 @@ const TodoController = (() => {
   const toggleOpen = (e) => {
     const target = e.target;
 
-    if (target.matches('.del-todo-btn, .del-todo-btn *, .fa-caret-up, .fa-caret-down, .checkmark, .checkmark *')) return;
+    if (target.matches(`
+      .del-todo-btn,
+      .del-todo-btn *,
+      .fa-caret-up,
+      .fa-caret-down,
+      .checkmark,
+      .checkmark *
+      `)) return;
+
     if (!target.matches('.todo, .todo *')) return;
-    
     const element = target.closest('li');
     if (element.classList.contains('active')) {
       closeTodo();
@@ -64,84 +65,75 @@ const TodoController = (() => {
       const id = element.dataset.id;
       openTodo(id);
     }
-  }
-  
+  };
   const openTodo = (id) => {
     TodosView.setActive(id);
-    
     const activeProject = Projects.getActive();
     const todo = activeProject.findTodo(id);
-    
     activeProject.setActiveTodo(todo);
     DetailsView.showDetails(todo);
-  }
-  
+  };
   const closeTodo = () => {
     DetailsView.hideDetails();
     TodosView.clearActiveTodo();
-  }
-  
+  };
   const addTodo = (e) => {
     e.preventDefault();
     const inputValues = AddTodoView.getFormInput();
     const proj = Projects.getActive();
-    
     proj.addTodo(
-      todo(
-        inputValues.title.value,
-        inputValues.description.value,
-        inputValues['due-date'].value
-      )
+        todo(
+            inputValues.title.value,
+            inputValues.description.value,
+            inputValues['due-date'].value,
+        ),
     );
-  
     TodosView.render(proj.todos);
     elements.addTodoForm.reset();
     Projects.saveToLocalStorage();
-  }
-  
+  };
   const deleteTodo = (target) => {
     if (!target.matches('.del-todo-btn, .del-todo-btn *')) return;
-    
     const id = target.closest('li').dataset.id;
     const project = Projects.getActive();
     project.removeTodo(id);
     TodosView.render(project.todos);
     closeTodo();
     Projects.saveToLocalStorage();
-  }
-  
+  };
   const changePriority = (e) => {
     const target = e.target;
     if (!target.matches('.fa-caret-up, .fa-caret-down')) return;
-      const id = target.closest('li').dataset.id;
-      const project = Projects.getActive();
-  
-      project.setTodoPriority(id, getDirection(target));
-      TodosView.render(project.todos);
-      Projects.saveToLocalStorage();
-  }
-  
+    const id = target.closest('li').dataset.id;
+    const project = Projects.getActive();
+    project.setTodoPriority(id, getDirection(target));
+    TodosView.render(project.todos);
+    Projects.saveToLocalStorage();
+  };
   const getDirection = (target) => {
     if (target.classList.contains('fa-caret-up')) {
       return 'up';
     } else {
       return 'down';
     }
-  }
-  
+  };
   const toggleCompleted = (e) => {
     const target = e.target;
-    
     if (!target.matches('.checkmark, .checkmark *')) return;
     const id = target.closest('li').dataset.id;
     const todo = Projects.getActive().findTodo(id);
     todo.completed ? todo.completed = false : todo.completed = true;
-  
     TodosView.toggleChecked(id);
     Projects.saveToLocalStorage();
-  }
-
-  return {addTodo, closeTodo, toggleOpen, deleteTodo, changePriority, toggleCompleted}
+  };
+  return {
+    addTodo,
+    closeTodo,
+    toggleOpen,
+    deleteTodo,
+    changePriority,
+    toggleCompleted,
+  };
 })();
 
 const DetailsController = (() => {
@@ -149,7 +141,7 @@ const DetailsController = (() => {
     if (e.target.id !== 'edit-btn') return;
     const todo = Projects.getActive().getActiveTodo();
     DetailsView.editDetails(todo);
-  }
+  };
 
   const saveEdit = (e) => {
     e.preventDefault();
@@ -157,16 +149,14 @@ const DetailsController = (() => {
     const inputValues = DetailsView.getFormInput();
     const activeProject = Projects.getActive();
     const todo = activeProject.getActiveTodo();
-    
     todo.title = inputValues['edit-title'].value;
     todo.dueDate = inputValues['edit-due-date'].value;
     todo.description = inputValues['edit-description'].value;
-    
     DetailsView.showDetails(todo);
     TodosView.render(activeProject.todos);
     TodosView.setActive(todo.id);
     Projects.saveToLocalStorage();
-  }
+  };
   return {editTodo, saveEdit};
 })();
 
@@ -174,9 +164,16 @@ const PopupController = (() => {
   let target;
   const openPopup = (e) => {
     target = e.target;
-    if (!target.matches('.del-project-btn, .del-project-btn *, .del-todo-btn, .del-todo-btn *')) return;
-    target.closest('ul').id === 'project-list' ? Popup.open('project') : Popup.open('to-do');
-  }
+    if (!target.matches(`
+      .del-project-btn,
+      .del-project-btn *,
+      .del-todo-btn,
+      .del-todo-btn *
+    `)) return;
+    target.closest('ul').id === 'project-list' ?
+    Popup.open('project') :
+    Popup.open('to-do');
+  };
 
   const handleDeletion = () => {
     Popup.close();
@@ -184,10 +181,12 @@ const PopupController = (() => {
     const todoNode = TodosView.triggerFade(id);
     todoNode.addEventListener('transitionend', (e) => {
       if (e.propertyName === 'border-top-color') {
-        target.closest('ul').id === 'project-list' ? ProjectController.deleteProject(target) : TodoController.deleteTodo(target);
+        target.closest('ul').id === 'project-list' ?
+        ProjectController.deleteProject(target) :
+        TodoController.deleteTodo(target);
       }
     });
-  }
+  };
   return {openPopup, handleDeletion};
 })();
 
@@ -200,30 +199,25 @@ const LoadingController = (() => {
     Projects.setActive(proj1);
     ProjectsView.select(proj1);
     TodosView.render(proj1.todos);
-  }
-  
+  };
+
   const loadProjects = (projects) => {
-    projects.forEach(projectItem => {
-      
+    projects.forEach((projectItem) => {
       const proj = Projects.add(project(projectItem.title));
       proj.id = projectItem.id;
-      projectItem.todos.forEach(todoItem => {
-        
+      projectItem.todos.forEach((todoItem) => {
         proj.addTodo(
-          todo(
-            todoItem.title,
-            todoItem.description,
-            todoItem.dueDate,
-            todoItem.completed
-          )
+            todo(
+                todoItem.title,
+                todoItem.description,
+                todoItem.dueDate,
+                todoItem.completed,
+            ),
         );
-        
-      })
-  
+      });
     });
     ProjectsView.render(Projects.get());
-  }
-  
+  };
   const init = () => {
     const projects = Projects.getFromLocalStorage();
     !projects ? firstTimeSetup() : loadProjects(projects);
